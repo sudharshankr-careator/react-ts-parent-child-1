636dfce7f146f879d8075aa46657e68ee546ca93
sudharshankr-careator <sudharshan.kr@careator.com>
2021-10-04 14:38:50 +0530

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-interface AppProps { }
+interface AppProps {}
 interface AppState {
   name: string;
 }
@@ -13,28 +13,37 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-interface AppProps { }
+interface AppProps {}
 interface AppState {
   name: string;
 }
 
 function App() {
-  const [name, setName]= useState('')
-  const [name1, setName1] = useState('Reddy')
-  const submit=(event)=> {
+  const [name, setName] = useState('');
+  const [name1, setName1] = useState('Reddy');
+  const submit = (event) => {
     event.preventDefault();
-    setName1(name)
-  }
- 
+    setName1(name);
+  };
+
   return (
     <div>
       <Hello name={name1} />
-      <form action="" onSubmit={(event)=> {submit(event)}}>
-        <input type="text" placeholder='Enter name' onChange={(e)=>setName(e.target.value)} />
-        <button type='submit' >submit</button>
+      <form
+        action=""
+        onSubmit={(event) => {
+          submit(event);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit">submit</button>
       </form>
     </div>
-  )
+  );
 }
 
 render(<App />, document.getElementById('root'));
